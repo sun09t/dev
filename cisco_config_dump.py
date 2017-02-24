23ee9d7d0b908578b430af52447f37309fc3c7f7
w sun <sun09t@gmail.com>
2017-02-24 14:44:28 -0500

    add cisco_config_dump.py

---
+++ cisco_config_dump.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2.7
+import Exscript.util.file as euf
+import Exscript.util.start as eus    
+import Exscript.util.match as eum     
+import os
+import time
+import datetime
+import re
+hosts = euf.get_hosts_from_file('hosts_test.txt')
+accounts = euf.get_accounts_from_file('accounts.cfg.lab')
+def dump_config(job, host, conn):
+		conn.execute('term len 0')       
+		conn.execute('show run')
+		config = conn.response.splitlines()
+		hostname = eum.first_match(conn, r'^hostname\s(.+)$')
+		if not hostname:
+			m = re.search(r"([\w-]+)", config[-1])
+			hostname = m.group(0)
+		cfg_file = 'network_configs/' + hostname.strip() + '.cfg'
+		for i in range(3):
+				config.pop(i)
+		config.pop(-0)
+		config.pop(-1)
+		with open(cfg_file, 'w') as f:
+				for line in config:
+						f.write(line + '\n')
+
+eus.start(accounts, hosts, dump_config, max_threads=5) 
